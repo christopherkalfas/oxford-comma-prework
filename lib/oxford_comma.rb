5a1926ecdc14396f05b5eb9462c103683a35dd33
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-11 21:01:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,8 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2 
     array.join(" and ")
-  elsif array.length == 3
-    array.join(" , ", " and ")
-  else array.length <= 4
-    array.join (","array.length -1, "and" )
+ 
+  else array.length >= 3
+    
   end
 end 
